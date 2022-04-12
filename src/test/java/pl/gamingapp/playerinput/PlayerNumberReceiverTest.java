f3aaacb454a87d563ca94964651a26f024bca689
Piotrek92 <piotrciolczyk992@gmail.com>
2022-04-12 22:07:14 +0200

    added new test to check if empty constructor initialization works correctly

--- src/test/java/pl/gamingapp/playerinput/PlayerNumberReceiverTest.java
+++ src/test/java/pl/gamingapp/playerinput/PlayerNumberReceiverTest.java
@@ -1,6 +1,15 @@
 package pl.gamingapp.playerinput;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class PlayerNumberReceiverTest {
 
+    @Test
+    public void creationOfEmptyConstructorOfClassPlayerNumberReceiver() {
+        PlayerNumbersReceiver pnr = new PlayerNumbersReceiver();
+        assertNotNull(pnr);
+    }
 
 }
