e69c2c836ae2aed17788902e800552462ea3c48d
Piotrek92 <piotrciolczyk992@gmail.com>
2022-04-13 23:08:27 +0200

    added initialization of array in LotteryReceiver constructor / inside w/o variables needed

--- src/main/java/pl/gamingapp/lotterydrawsystem/LotteryRandomNumbersReceiver.java
+++ src/main/java/pl/gamingapp/lotterydrawsystem/LotteryRandomNumbersReceiver.java
@@ -4,5 +4,7 @@ public class LotteryRandomNumbersReceiver {
     private final int AMOUNT_OF_LUCKY_DRAWN_NUMBERS = 6;
     private int[] luckyDrawnNumbers;
 
-    public LotteryRandomNumbersReceiver() {}
+    public LotteryRandomNumbersReceiver() {
+        this.luckyDrawnNumbers = new int[AMOUNT_OF_LUCKY_DRAWN_NUMBERS];
+    }
 }
