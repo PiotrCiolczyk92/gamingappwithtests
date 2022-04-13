2e351f63249fc832b582722a70593c8bb525c253
Piotrek92 <piotrciolczyk992@gmail.com>
2022-04-13 23:07:14 +0200

    added variables to LotteryReceiver class

--- src/main/java/pl/gamingapp/lotterydrawsystem/LotteryRandomNumbersReceiver.java
+++ src/main/java/pl/gamingapp/lotterydrawsystem/LotteryRandomNumbersReceiver.java
@@ -1,4 +1,8 @@
 package pl.gamingapp.lotterydrawsystem;
 
 public class LotteryRandomNumbersReceiver {
+    private final int AMOUNT_OF_LUCKY_DRAWN_NUMBERS = 6;
+    private int[] luckyDrawnNumbers;
+
+    public LotteryRandomNumbersReceiver() {}
 }
--- src/test/java/pl/gamingapp/lotterydrawsystem/LotteryRandomNumbersReceiverTest.java
+++ src/test/java/pl/gamingapp/lotterydrawsystem/LotteryRandomNumbersReceiverTest.java
@@ -1,4 +1,13 @@
-import static org.junit.jupiter.api.Assertions.*;
+package pl.gamingapp.lotterydrawsystem;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class LotteryRandomNumbersReceiverTest {
-  
+
+    @Test
+    public void checkIfEmptyConstructorForThisClassWorksCorrectly() {
+        assertNotNull(new LotteryRandomNumbersReceiver());
+    }
 }
